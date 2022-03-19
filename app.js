699f7ca9ea1bf11c4285449594320b2db5c068db
kameHame HA <80411896+kamehame-ha@users.noreply.github.com>
2022-03-19 20:27:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ const formidable = require('formidable')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, dir)))
 
 app.get('/', (req, res) => {
